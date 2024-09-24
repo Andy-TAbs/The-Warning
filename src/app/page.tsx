e22f167da629fb33e1050a320c1f11ab3c3a9fee
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-09-24 13:50:11 -0600

    Add  project

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <header className="header">
+        <div className="logo"></div>
+        <div className="menu"></div>
+    </header>
+
+    <div className="imagen-container">
+        <div className="imagen-con-nombre">
+            <a className="imagen-button">
+                <img className="imagen" src="imagenes/dany0.jpg" alt="Imagen 1" />
+            </a>
+        </div>
+        <div className="imagen-con-nombre">
+            <a className="imagen-button">
+                <img className="imagen" src="imagenes/pau0.jpg" alt="Imagen 2" />
+            </a>
+        </div>
+        <div className="imagen-con-nombre">
+            <a className="imagen-button">
+                <img className="imagen" src="imagenes/ale0.jpg" alt="Imagen 3" />
+            </a>
+        </div>
+        <div className="button-container-inicio">
+            <div className="imagen-banda">
+                <a className="boton-banda">
+                    <img className="imagen" src="imagenes/logo.webp" alt="imagen 4" />
+                </a>
+            </div>
+        </div>
+    </div>
+    </div>
+  );
+}
